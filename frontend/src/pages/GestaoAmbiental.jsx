416be4b852ee96e6f1e3d87a2821288ee4ba5a04
williams4803 <wcjesus38@gmail.com>
2026-08-15 19:12:56 -0300

    Atualizar GestaoAmbiental com modificações recentes

--- frontend/src/pages/GestaoAmbiental.jsx
+++ frontend/src/pages/GestaoAmbiental.jsx
@@ -38,9 +38,8 @@ function GestaoAmbiental() {
       <h1 style={styles.titulo}>Gestão Ambiental</h1>
 
       <p style={styles.descricao}>
-        Pós-graduação em Gestão Ambiental pela Universidade Estácio de Sá, /n
-        com foco em sustentabilidade, prevenção de riscos e boas práticas ambientais /n
-        aplicadas ao ambiente corporativo.
+        
+          Instituição: Universidade Estácio de Sá. Duração: 12 meses. Carga horária: 360 horas. Modalidade: EAD. Certificação: Pós-graduação lato sensu em Gestão Ambiental. sustentabilidade, prevenção de riscos e boas práticas ambientais aplicadas ao ambiente corporativo.
 
       </p>
 
